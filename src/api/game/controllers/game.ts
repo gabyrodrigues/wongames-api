1ff1eefc9392643c3b0290ba2da86ab45d0c1421
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-06 19:55:58 -0300

    creating basic controller and service to find game by category

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,6 +2,15 @@
  * game controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      console.log("Starting to populate");
+      await strapi.service("api::game.game").populate(ctx.query);
+      ctx.send("Finished populating!");
+    },
+  })
+);
