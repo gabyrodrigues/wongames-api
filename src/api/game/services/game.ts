ec82ba8b1f676e7d91c3380d96bc4f2d1f4ba3cd
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-06 20:13:13 -0300

    fetching gog products list in Game Service

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,14 +1,17 @@
 /**
  * game service
  */
-
+import axios from "axios";
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::game.game", () => ({
   async populate(params) {
-    const cat = await strapi.service("api::category.category").find({
-      filters: { name: params.category },
-    });
-    console.log(cat);
+    const gogApiUrl = `https://gog.com/games/ajax/filtered?mediaType=game`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(products[0]);
   },
 }));
